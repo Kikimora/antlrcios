b9a26438f7eda9b6a6275393149bf951ef972412
Andrey Verbin <andrey@studiomobile.ru>
2012-10-08 18:51:11 +0400

    Remove build directory from repo

--- antlrc/build/antlrc.build/Debug-iphoneos/antlrc.build/Objects-normal/armv7/antlr3exception.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/averbin/Downloads/libantlr3c-3.4/antlrc/../src/antlr3exception.c \
-  /Users/averbin/Downloads/libantlr3c-3.4/antlrc/../include/antlr3exception.h \
-  /Users/averbin/Downloads/libantlr3c-3.4/antlrc/../include/antlr3defs.h \
-  /Users/averbin/Downloads/libantlr3c-3.4/antlrc/../include/antlr3errors.h \
-  ../antlr3config.h \
-  /Users/averbin/Downloads/libantlr3c-3.4/antlrc/../include/antlr3interfaces.h \
-  /Users/averbin/Downloads/libantlr3c-3.4/antlrc/../include/antlr3convertutf.h
--- antlrc/build/antlrc.build/Release-iphoneos/antlrc.build/Objects-normal/armv7/antlr3intstream.d
+++
@@ -1,9 +0,0 @@
-dependencies: \
-  /Users/averbin/Downloads/libantlr3c-3.4/antlrc/../src/antlr3intstream.c \
-  /Users/averbin/Downloads/libantlr3c-3.4/antlrc/../include/antlr3intstream.h \
-  /Users/averbin/Downloads/libantlr3c-3.4/antlrc/../include/antlr3defs.h \
-  /Users/averbin/Downloads/libantlr3c-3.4/antlrc/../include/antlr3errors.h \
-  ../antlr3config.h \
-  /Users/averbin/Downloads/libantlr3c-3.4/antlrc/../include/antlr3interfaces.h \
-  /Users/averbin/Downloads/libantlr3c-3.4/antlrc/../include/antlr3convertutf.h \
-  /Users/averbin/Downloads/libantlr3c-3.4/antlrc/../include/antlr3commontoken.h
